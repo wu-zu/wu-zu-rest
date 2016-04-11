ab26585865682d8ed706cde9488c485bb35d3661
Kyunghun Jeon <penpen787@gmail.com>
2016-04-11 10:00:08 +0900

    FK pageable 구현완료

--- wu-zu-rest-backend/src/main/java/com/wuzu/board/repository/CommentRepository.java
+++ wu-zu-rest-backend/src/main/java/com/wuzu/board/repository/CommentRepository.java
@@ -1,10 +1,15 @@
 package com.wuzu.board.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wuzu.board.domain.Comment;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 //@RepositoryRestResource(collectionResourceRel = "comments", path = "comments")
 public interface CommentRepository extends JpaRepository<Comment, Integer>{ 
 
+    List<Comment> findByArticle_Id(@Param(value = "articleID") int articleID, Pageable pageable);
 }
